677f875d741b963139a789128ad284ac44662f72
Bradley Harper <bradharper123@hotmail.com>
2019-03-25 12:29:59 -0500

    yes, another route modification

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ export default new Router({
     'mode': 'history',
     'routes': [
         {
-            'path': '/',
+            'path': '/home',
             'name': 'Home',
             'component': Home
         },
