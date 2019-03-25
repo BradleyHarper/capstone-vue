c862ac6c21eab94189a9931e3d79f24d2cdc1354
Bradley Harper <bradharper123@hotmail.com>
2019-03-25 12:25:56 -0500

    Edited home route :sparkles:

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ export default new Router({
     'mode': 'history',
     'routes': [
         {
-            'path': '/home',
+            'path': '/',
             'name': 'Home',
             'component': Home
         },
