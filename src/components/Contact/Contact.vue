e83c70b469dd928207467c811fd5defe9251975f
BradleyHarper <bradharper123@hotmail.com>
2020-06-26 13:46:17 -0500

    cut new feature branch

--- src/components/Contact/Contact.vue
+++ src/components/Contact/Contact.vue
@@ -5,19 +5,10 @@
     <p>
         link:   Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores porro voluptatem excepturi cumque debitis? Nam ipsa et aperiam, qui officiis, itaque asperiores quia repudiandae earum porro eos minus pariatur placeat.
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod placeat libero et porro distinctio sit itaque minima harum labore deleniti voluptates sunt tempora voluptatibus architecto facilis, quas modi neque qui!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nemo quasi voluptatibus consectetur minima cupiditate necessitatibus, animi similique tempora minus. Dolore beatae omnis, nulla molestias possimus provident distinctio numquam aliquid?
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia, ab libero repellat ducimus facilis accusamus sint. Quis culpa illo voluptas nobis atque quisquam saepe, a dignissimos pariatur quidem dolorum minus!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aperiam qui, enim tenetur ut inventore aspernatur maxime quidem possimus culpa ad vel nulla laboriosam. Consequuntur consectetur debitis deleniti fuga velit!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam tempora distinctio et, ea earum adipisci quisquam quo dolorem ullam ipsa iure odio est, molestias illo vel perferendis mollitia magnam nam!
-
     </p>
     <h2>Anything that we missed?</h2>
     <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur nesciunt ducimus ab animi suscipit! Quam, placeat! Sed blanditiis doloremque eos fuga, hic adipisci quaerat quam dolorem repellat quod voluptas! Beatae.
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus animi cumque ipsam at veritatis reiciendis voluptates accusantium, exercitationem officia facilis delectus incidunt illo eligendi, obcaecati non corrupti modi. Totam, iure.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem culpa doloremque, beatae sunt quibusdam eveniet! Soluta, assumenda fugiat. Placeat culpa officia facilis beatae explicabo ipsum labore repellendus dolores sapiente temporibus?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid voluptate ducimus dolorum, harum magni quam reprehenderit voluptatem unde molestiae obcaecati, optio eaque facilis nam! Autem rerum voluptatum deserunt eligendi ipsam!
-
     </p>
     </div>
 </template>
--- src/components/Instructions/Instructions.vue
+++ src/components/Instructions/Instructions.vue
@@ -1,16 +1,12 @@
 <template>
 <div class="page">
-    <h1>Instructions</h1> 
+    <h1>Instructions</h1>
     <div id="instructions" >
         <h2>Connect the feed and watering unit to the internet.</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam consequatur enim ea excepturi earum dicta, magni corrupti maiores expedita praesentium fugiat, nam distinctio perspiciatis animi soluta nobis repudiandae. Eum, facilis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, voluptatibus quod. Rerum exercitationem eius tenetur nesciunt iste cum tempore corrupti autem quam alias, reprehenderit quod eum cumque repellendus velit aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque illum, quasi dolores fuga explicabo consequatur quidem voluptas et doloremque esse saepe alias, quibusdam repudiandae natus quisquam! Autem neque ratione molestias.</p>
         <h2>Congrats! thats all for now.</h2>
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sit ullam at repellendus. Amet, molestiae laboriosam. Quam nesciunt non id iste, voluptas dolorum eligendi pariatur natus dolor soluta quia beatae.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate pariatur laborum quos repellendus, quidem, quis, esse ex asperiores earum magni nostrum! Eum, doloremque earum quis cumque explicabo voluptatibus ad adipisci!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe autem esse id obcaecati? Qui adipisci vitae molestias necessitatibus. Odio doloribus officia, animi unde laboriosam culpa doloremque laborum cum voluptas tempora?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero culpa, numquam beatae odit id ipsam, odio tenetur aut labore, nihil officiis optio ad. Laboriosam voluptates veniam totam quas est provident?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut ut quo distinctio incidunt rem ea saepe quis! Provident quis quas officiis quisquam sunt maxime nisi blanditiis assumenda nesciunt culpa.
         </p>
     </div>
 </div>
